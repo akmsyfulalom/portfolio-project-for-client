8cab3087a33a130c648f25cfae0a8929284c5c5c
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-06-27 15:26:35 +0600

    hero section updated

--- src/components/header/Hero.jsx
+++ src/components/header/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
             <div>
             <h1 className="mb-5 text-2xl font-bold">Hello there</h1>
               <h1 className="text-5xl font-bold">I'm Alonzo Johnson</h1>
-              <p className="py-3">I'm God's child! I'm a sinner fighting the good fight with Love and prayer.</p>
+              <p className="py-3">I'm God's child!, sinner fighting the good fight with Love and prayer.</p>
               <Link href="/about" className="btn btn-primary">Learn more</Link>
             </div>
         </div>
