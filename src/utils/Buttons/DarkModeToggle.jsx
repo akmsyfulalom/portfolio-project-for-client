b4ee789b77a7e8dcdba696f64146f1e9cf1a7240
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-06-27 17:58:11 +0600

    extra componets remove now

--- src/utils/Buttons/DarkModeToggle.jsx
+++ src/utils/Buttons/DarkModeToggle.jsx
@@ -7,7 +7,7 @@ const DarkModeToggle = () => {
 
   return (
     <button
-      className=" bg-gray-200 dark:bg-gray-900 text-gray-600 dark:text-gray-300 px-4 py-2 rounded-md"
+      className=" bg-gray-200 dark:bg-gray-800 text-gray-600 dark:text-gray-300 px-4 py-2 rounded-md"
       onClick={toggleDarkMode}
     >
       Light Theme
