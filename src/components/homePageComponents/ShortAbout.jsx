2c2d8563e990d5683c92d180bb159eb90756bbac
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-06-27 18:22:06 +0600

    extra componets remove now

--- src/components/homePageComponents/ShortAbout.jsx
+++ src/components/homePageComponents/ShortAbout.jsx
@@ -13,7 +13,7 @@ const ShortAbout = () => {
       <p className="my-5">A highly versatile artist, Rachel is an American singer who got her start her career in the 2000s.</p>
       <span className="my-5">Born on April 22, 1986, in El Centro, California, Rachel rose to stardom as part of a singing act with brother Ban Calen in the 2000s, hitting No. 1 with the single «Make Me Happy. Rachel had established a solo career of her own as well, having chart-toppers like Gypsies, Tramps and Thieves, Half-Breed and Dark Lady. Rachel also found more music success with rock oriented tracks in the 15s and a global dance hit Believe and My Dreams in the late 16s. She released Closer to the Truth, her first studio album in 10 years, in 2016, which offered fans more dance hits.</span>
       <p>Alonzo Johnson</p>
-      <p>Azo4life</p>
+      
       </div>
       <Link href="/about" className="btn btn-primary">Know more</Link>
     </div>
