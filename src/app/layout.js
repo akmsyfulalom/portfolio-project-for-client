cf1fcd3451a6a72f28aa4506ef48d7f2f802b1d9
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-06-27 15:48:13 +0600

    remove before hero section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,52 +1,3 @@
-// // "use client"
-// // import React, { useEffect } from 'react';
-// // import Navbar from '@/components/navbar/Navbar';
-// // import Footer from '@/components/footer/Footer';
-// // import { useRouter } from 'next/router';
-
-// "use client"
-// import React, { useEffect } from 'react';
-// import Navbar from '@/components/navbar/Navbar';
-// import Footer from '@/components/footer/Footer';
-
-// const RootLayout = ({ children }) => {
-
-//   useEffect(() => {
-//     const handleRouteChange = () => {
-//       // Remove the dark theme when navigating to a new page
-//       document.documentElement.classList.remove('dark');
-//     };
-
-//     router.events.on('routeChangeStart', handleRouteChange);
-
-//     return () => {
-//       router.events.off('routeChangeStart', handleRouteChange);
-//     };
-//   }, []);
-
-//   const toggleTheme = () => {
-//     document.documentElement.classList.toggle('dark');
-//   };
-
-//   return (
-//     <div className="bg-white dark:bg-gray-900 text-black dark:text-white">
-//       <button
-//         className="fixed right-4 bottom-4 bg-gray-200 dark:bg-gray-800 text-gray-600 dark:text-gray-300 px-4 py-2 rounded-md"
-//         onClick={toggleTheme}
-//       >
-//         Toggle Theme
-//       </button>
-//       <Navbar />
-//       {children}
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default RootLayout;
-
-
-
 "use client"
 import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
@@ -64,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar className="z"/>
         {children}
         <Footer/>
         </body>
