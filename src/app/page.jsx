fe2855e08280cfe8187ed0ded8970a56ec9f4254
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-06-27 16:24:24 +0600

    retest

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,3 @@
-// "use client"
-// import { useSpring, animated } from 'react-spring';
-// import HeroSection from '@/components/header/HeroSection';
-// import ShortAbout from '@/components/homePageComponents/ShortAbout';
-// import YoutubeSection from '@/components/homePageComponents/YoutubeSection';
-
-// export default function Home() {
-//   const heroSectionAnimation = useSpring({ opacity: 1, from: { opacity: 0 }, delay: 500 });
-//   const shortAboutAnimation = useSpring({ opacity: 1, from: { opacity: 0 }, delay: 1000 });
-//   const youtubeSectionAnimation = useSpring({ opacity: 1, from: { opacity: 0 }, delay: 1500 });
-
-//   return (
-//     <div>
-//       <animated.div style={heroSectionAnimation}>
-//         <HeroSection />
-//       </animated.div>
-
-//       <animated.div style={shortAboutAnimation}>
-//         <ShortAbout />
-//       </animated.div>
-
-//       <animated.div style={youtubeSectionAnimation}>
-//         <YoutubeSection />
-//       </animated.div>
-//     </div>
-//   );
-// }
-
 import Hero from '@/components/header/Hero'
 import ShortAbout from '@/components/homePageComponents/ShortAbout'
 import YoutubeSection from '@/components/homePageComponents/YoutubeSection'
@@ -34,11 +6,9 @@ import YoutubeSection from '@/components/homePageComponents/YoutubeSection'
 export default function Home() {
   return (
   <div>
-    
      <Hero/>
      <ShortAbout/>
      <YoutubeSection/>
-    
   </div>
   )
 }
